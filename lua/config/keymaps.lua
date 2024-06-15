6c5ce496620d35de8cb8c0d44f37699d4b97ad11
Taice <tai.stanic@gmail.com>
2024-06-15 20:26:45 +0200

    rr

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -5,3 +5,4 @@
 -- file browser
 vim.keymap.set("n", "<leader>eh", ":Telescope file_browser<CR>", { noremap = true })
 vim.keymap.set("n", "<leader>eb", ":Telescope file_browser path=%:p:h select_buffer=true<CR>", { noremap = true })
+vim.keymap.set("n", "<leader>fw", ":w<CR>")
